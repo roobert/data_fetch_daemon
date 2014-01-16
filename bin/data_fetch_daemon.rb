4dfdaddc66005b860770eccb29f532ca0f15ed4b
Rob Wilson <roobert@gmail.com>
2014-01-16 19:51:41 +0000

    change location of config file

--- bin/data_fetch_daemon.rb
+++ bin/data_fetch_daemon.rb
@@ -1,18 +1,16 @@
 #!/usr/bin/env ruby
 
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '../lib'))
+
 require 'rubygems'
 require 'backports'
 require 'yaml'
 require 'awesome_print'
 require 'active_support/core_ext/hash/indifferent_access'
-
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '../lib'))
-
 require 'data_fetch_daemon'
 
-config = HashWithIndifferentAccess.new(YAML.load_file('config.yaml'))
-
-fetch = DataFetchDaemon.new(config)
+config = HashWithIndifferentAccess.new(YAML.load_file(File.join(File.dirname(__FILE__), '../config.yaml')))
+fetch  = DataFetchDaemon.new(config)
 
 fetch.configure
 fetch.run
